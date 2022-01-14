42e6a9a49ced4e941f7cdc81cbff478b57bf7fce
Leif Denby <leif@denby.eu>
2022-01-14 11:51:24 +0000

    refactor for defining tiles-kind for sampling

--- convml_data/pipeline/tiles.py
+++ convml_data/pipeline/tiles.py
@@ -1,42 +1,97 @@
 from pathlib import Path
 
 import luigi
-import matplotlib.pyplot as plt
 import regridcart as rc
 
 from .. import DataSource
 from ..sampling import domain as sampling_domain
-from ..utils.domain_images import align_axis_x, rgb_image_from_scene_data
-from ..utils.luigi import ImageTarget, XArrayTarget
-from . import GenerateSceneIDs
-from .aux_sources import CheckForAuxiliaryFiles
+from ..utils.domain_images import rgb_image_from_scene_data
+from ..utils.luigi import DBTarget, XArrayTarget
+from . import triplets
 from .sampling import CropSceneSourceFiles, SceneSourceFiles, _SceneRectSampleBase
-from .utils import SceneBulkProcessingBaseTask
 
 
-def _plot_scene_aux(da_aux, img, **kwargs):
-    fig, axes = plt.subplots(nrows=2, figsize=(10.0, 8.0), sharex=True)
-    img_extent = [
-        da_aux.x.min().item(),
-        da_aux.x.max().item(),
-        da_aux.y.min().item(),
-        da_aux.y.max().item(),
-    ]
-    axes[0].imshow(img, extent=img_extent)
-    axes[1].imshow(img, extent=img_extent)
-    if da_aux.name == "HT":
-        # cloud-top height [m]
-        kwargs["vmin"] = 0.0
-        kwargs["vmax"] = 14.0e3
-    da_aux.plot(ax=axes[1], y="y", cmap="nipy_spectral", **kwargs)
-    align_axis_x(ax=axes[0], ax_target=axes[1])
-    return fig, axes
+class TilesInScene(luigi.Task):
+    data_path = luigi.Parameter(default=".")
+    scene_id = luigi.Parameter()
+    tiles_kind = luigi.Parameter()
+
+    def requires(self):
+        if self.tiles_kind == "triplets":
+            return triplets.TripletSceneSplits(data_path=self.data_path)
+
+        raise NotImplementedError(self.tiles_kind)
+
+    def run(self):
+        tiles_per_scene = self.input().open()
+        # we will write an empty file since we don't need to sample tiles
+        # from this scene
+        scene_tiles_meta = tiles_per_scene.get(self.scene_id, {})
+        self.output().write(scene_tiles_meta)
+
+    def output(self):
+        p = Path(self.data_path) / "tiles"
+        return DBTarget(path=p, db_type="yaml", db_name="f{self.scene_id}_tiles")
 
 
-class SceneRegriddedData(_SceneRectSampleBase):
+class SceneTileLocations(luigi.Task):
     """
-    Regrid the scene source data to a fixed Cartesian resolution
+    For a given scene work out the sampling locations of all the tiles in it
     """
+
+    data_path = luigi.Parameter(default=".")
+    scene_id = luigi.Parameter()
+    tiles_kind = luigi.Parameter()
+
+    @property
+    def data_source(self):
+        return DataSource.load(path=self.data_path)
+
+    def requires(self):
+        reqs = dict(
+            tiles_meta=TilesInScene(
+                scene_id=self.scene_id,
+                data_path=self.data_path,
+                tiles_kind=self.tiles_kind,
+            ),
+        )
+
+        domain = self.data_source.domain
+        if isinstance(domain, sampling_domain.SourceDataDomain):
+            reqs["scene_source_data"] = SceneSourceFiles(
+                scene_id=self.scene_id, data_path=self.data_path
+            )
+
+        return reqs
+
+    def run(self):
+        tiles_meta = self.input()["tiles_meta"].open()
+
+        if len(tiles_meta) > 0:
+            # not all scenes have to have tiles in them (if for example we're
+            # sampling fewer tiles that we have scenes)
+            if self.tiles_kind == "triplets":
+                domain = self.data_source.domain
+                if isinstance(domain, sampling_domain.SourceDataDomain):
+                    ds_scene = self.input()["scene_source_data"].open()
+                    domain = domain.generate_from_dataset(ds=ds_scene)
+                tile_locations = triplets.sample_triplet_tile_locations(
+                    tiles_meta=tiles_meta, domain=domain, data_source=self.data_source
+                )
+            else:
+                raise NotImplementedError(self.tiles_kind)
+
+        Path(self.output().fn).parent.mkdir(exist_ok=True, parents=True)
+        self.output().write(tile_locations)
+
+    def output(self):
+        name = f"tile_locations.{self.scene_id}"
+        p = Path(self.data_path) / "triplets"
+        return DBTarget(path=p, db_type="yaml", db_name=name)
+
+
+class SceneTilesData(_SceneRectSampleBase):
+    tiles_kind = luigi.Parameter()
 
     @property
     def data_source(self):
@@ -50,118 +105,116 @@ class SceneRegriddedData(_SceneRectSampleBase):
             reqs["source_data"] = SceneSourceFiles(
                 scene_id=self.scene_id,
                 data_path=self.data_path,
-                aux_product=self.aux_product,
             )
         else:
             reqs["source_data"] = CropSceneSourceFiles(
                 scene_id=self.scene_id,
                 data_path=self.data_path,
                 pad_ptc=self.crop_pad_ptc,
-                aux_product=self.aux_product,
             )
 
-        if self.aux_product is not None:
-            reqs["base"] = SceneRegriddedData(
-                scene_id=self.scene_id,
-                data_path=self.data_path,
-                crop_pad_ptc=self.crop_pad_ptc,
-            )
+        reqs["tile_locations"] = SceneTileLocations(
+            data_path=self.data_path, scene_id=self.scene_id
+        )
 
         return reqs
 
-    @property
-    def scene_resolution(self):
-        data_source = self.data_source
-        if (
-            "resolution" not in data_source.sampling
-            or data_source.sampling.get("resolution") is None
-        ):
-            raise Exception(
-                "To produce isometric grid resampling of the source data please "
-                "define the grid-spacing by defining `resolution` (in meters/pixel) "
-                "in the `sampling` part of the data source meta information"
-            )
-        return data_source.sampling["resolution"]
-
     def run(self):
-        domain_output = self.output()
+        inputs = self.input()
+        source_data_input = inputs["source_data"]
+        # for cropped fields the parent task returns a dictionary so that
+        # we can have the rendered image too (if that has been produced)
+        if isinstance(source_data_input, dict):
+            da_src = source_data_input["data"].open()
+        else:
+            da_src = source_data_input.open()
+
+        domain = self.data_source.domain
+        if isinstance(domain, sampling_domain.SourceDataDomain):
+            domain = domain.generate_from_dataset(ds=da_src)
+
         data_source = self.data_source
+        dx = data_source.sampling["resolution"]
 
-        da_src = None
-        if not domain_output["data"].exists():
-            inputs = self.input()
-            da_src = inputs["source_data"]["data"].open()
+        for tile_identifier, tile_domain in self.tile_domains:
+            da_tile = rc.resample(domain=tile_domain, da=da_src, dx=dx)
+            tile_output = self.output()[tile_identifier]
+            tile_output["data"].write(da_tile)
 
-            domain = self.data_source.domain
-            if isinstance(domain, sampling_domain.SourceDataDomain):
-                domain = domain.generate_from_dataset(ds=da_src)
-
-            dx = self.scene_resolution
-
-            if self.aux_product is None:
-                method = "bilinear"
-            else:
-                method = "nearest_s2d"
-            da_domain = rc.resample(domain=domain, da=da_src, dx=dx, method=method)
-            Path(domain_output["data"].fn).parent.mkdir(exist_ok=True, parents=True)
-            domain_output["data"].write(da_domain)
-        else:
-            da_domain = domain_output["data"].open()
-
-        if self.aux_product is not None:
-            img_domain = self.input()["base"]["image"].open()
-            fig, _ = _plot_scene_aux(da_aux=da_domain, img=img_domain)
-            fig.savefig(domain_output["image"].fn)
-        else:
-            # need the scene attrs to generate a rgb image
-            if da_src is None:
-                da_src = self.input()["source_data"]["data"].open()
-            img_domain = rgb_image_from_scene_data(
-                data_source=data_source, da_scene=da_domain, src_attrs=da_src.attrs
+            img_tile = rgb_image_from_scene_data(
+                data_source=data_source, da_scene=da_tile, src_attrs=da_src.attrs
             )
-            img_domain.save(str(domain_output["image"].fn))
+            img_tile.save(str(tile_output["image"].fn))
+
+    @property
+    def tile_identifier_format(self):
+        if self.tiles_kind == "triplets":
+            tile_identifier_format = triplets.TILE_IDENTIFIER_FORMAT
+        else:
+            raise NotImplementedError(self.tiles_kind)
+
+        return tile_identifier_format
+
+    @property
+    def tile_domains(self):
+        tiles_meta = self.input()["tile_locations"].open()
+
+        for tile_meta in tiles_meta:
+            tile_domain = rc.deserialise_domain(tile_meta["loc"])
+            tile_identifier = self.tile_identifier_format.format(**tile_meta)
+
+            yield tile_identifier, tile_domain
 
     def output(self):
-        scene_data_path = Path(self.data_path) / "rect"
+        if not self.input()["tile_locations"].exists():
+            return luigi.LocalTarget("__fakefile__.nc")
 
-        if self.aux_product is not None:
-            scene_data_path = scene_data_path / "aux" / self.aux_product
+        tiles_meta = self.input()["tile_locations"].open()
 
-        fn_data = f"{self.scene_id}.nc"
-        fn_image = f"{self.scene_id}.png"
-        return dict(
-            data=XArrayTarget(str(scene_data_path / fn_data)),
-            image=ImageTarget(str(scene_data_path / fn_image)),
-        )
+        tile_data_path = Path(self.data_path) / "triplets"
+
+        outputs = {}
+
+        for tile_meta in tiles_meta:
+            tile_identifier = self.tile_identifier_format.format(**tile_meta)
+            fn_data = f"{tile_identifier}.nc"
+            fn_image = f"{tile_identifier}.png"
+            outputs[tile_identifier] = dict(
+                data=XArrayTarget(str(tile_data_path / fn_data)),
+                image=luigi.LocalTarget(str(tile_data_path / fn_image)),
+            )
+        return outputs
 
 
-class GenerateRegriddedScenes(SceneBulkProcessingBaseTask):
+class GenerateTiles(luigi.Task):
+    """
+    Generate all tiles across all scenes. First which tiles to generate per
+    scene is worked out (the method is dependent on the sampling strategy of
+    the tiles, for example triplets or along trajectories) and second
+    `SceneTilesData` is invoked to generate tiles for each scene.
+    """
+
     data_path = luigi.Parameter(default=".")
-    TaskClass = SceneRegriddedData
+    tiles_kind = luigi.Parameter()
 
-    aux_product = luigi.OptionalParameter(default=None)
-
-    def _get_task_class_kwargs(self, scene_ids):
-        if self.aux_product is None:
-            return {}
-
-        return dict(aux_product=self.aux_product)
+    @property
+    def data_source(self):
+        return DataSource.load(path=self.data_path)
 
     def requires(self):
-        if self.TaskClass is None:
-            raise Exception(
-                "Please set TaskClass to the type you would like"
-                " to process for every scene"
-            )
-        if self.aux_product is None:
-            TaskClass = GenerateSceneIDs
-        else:
-            TaskClass = CheckForAuxiliaryFiles
+        if self.tiles_kind == "triplets":
+            return triplets.TripletSceneSplits(data_path=self.data_path)
 
-        return TaskClass(data_path=self.data_path, **self._get_scene_ids_task_kwargs())
+        raise NotImplementedError(self.tiles_kind)
 
-    def _get_scene_ids_task_kwargs(self):
-        if self.aux_product is None:
-            return {}
+    def run(self):
+        tiles_per_scene = self.input().open()
 
-        return dict(product_name=self.aux_product)
+        tasks_tiles = {}
+        for scene_id, tiles_meta in tiles_per_scene.items():
+            if len(tiles_meta) > 0:
+                tasks_tiles[scene_id] = SceneTilesData(
+                    scene_id=scene_id, tiles_kind=self.tiles_kind
+                )
+
+        yield tasks_tiles
