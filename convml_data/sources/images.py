7e00843e3153a7f8b03e4e28974717b4af0355b4
Leif Denby <leif@denby.eu>
2022-03-09 20:38:04 +0000

    implement default scaling for brightness temperature images

--- convml_data/sources/images.py
+++ convml_data/sources/images.py
@@ -8,10 +8,11 @@ from PIL import Image
 from ..sources import goes16
 
 
-def make_rgb(da, alpha=0.5, invert_values=False, **coord_components):
+def make_rgb(da, alpha=0.5, invert_values=False, scaling=None, **coord_components):
     """
     turn three components along a particular coordinate into RGB values by
-    scaling each by its max and min-values in the dataset
+    scaling each by its max and min-values in the dataset. If `scaling` isn't
+    provided then the min-max for each component will be used for scaling
     >> make_rgb(da, emb_dim=[0,1,2])
     """
     if len(coord_components) != 1:
@@ -26,20 +27,8 @@ def make_rgb(da, alpha=0.5, invert_values=False, **coord_components):
             f"You should provide exactly three indexes of the `{v_dim}` coordinate to turn into RGB."
             f"{coord_components} was supplied"
         )
-    elif v_dim not in da.dims:
+    if v_dim not in da.dims:
         raise Exception(f"The `{v_dim}` coordinate wasn't found the provided DataArray")
-
-    if invert_values:
-
-        def scale_zero_one(v):
-            return 1.0 - (v - v.min()) / (v.max() - v.min())
-
-    else:
-
-        def scale_zero_one(v):
-            return (v - v.min()) / (v.max() - v.min())
-
-    scale = scale_zero_one
 
     all_dims = da.dims
     x_dim, y_dim = list(filter(lambda d: d != v_dim, all_dims))
@@ -51,34 +40,56 @@ def make_rgb(da, alpha=0.5, invert_values=False, **coord_components):
     )
 
     def _make_component(da_):
+        # the alpha channel is the 4th channel and should just be set to a constant
         if da_.rgba.data == 3:
             return alpha * np.ones_like(da_)
+
+        # otherwise we will extract along the provided coordinate and scale
+        # appropriately
+        component = dim_idxs[da_.rgba.item()]
+        vals = da.sel({v_dim: component}).values
+
+        if scaling is None:
+            v_min, v_max = vals.min(), vals.max()
         else:
-            return scale(da.sel({v_dim: dim_idxs[da_.rgba.item()]}).values)
+            v_min, v_max = scaling
+        v_range = v_max - v_min
+
+        v_scaled = (vals - v_min) / v_range
+
+        if invert_values:
+            v_scaled = 1.0 - v_scaled
+
+        return v_scaled
 
     da_rgba = da_rgba.groupby("rgba").apply(_make_component)
 
     return da_rgba
 
 
-def rgb_image_from_scene_data(source_name, product, da_scene, **kwargs):
+def rgb_image_from_scene_data(source_name, product, da_scene):
     if source_name == "goes16":
         if product == "truecolor_rgb" and "bands" in da_scene.coords:
             img_domain = goes16.satpy_rgb.rgb_da_to_img(da=da_scene)
-        elif product.startswith("multichannel__") or product.startswith(
-            "singlechannel__"
-        ):
-            if product.startswith("multichannel__"):
-                channels = list(goes16.parse_product_shorthand(product).keys())
-                # TODO: for now we will invert the Radiance channel values when
-                # creating RGB images from them
-                da_rgba = make_rgb(
-                    da_scene, channel=channels, invert_values=True, alpha=1.0
-                )
-            elif product.startswith("singlechannel__"):
-                raise NotImplementedError(product)
-            else:
-                raise NotImplementedError(product)
+        elif product.startswith("multichannel__"):
+            channels = goes16.parse_product_shorthand(product)
+
+            # check if we're using brightness temperature for all channels
+            for _, prefix in channels.items():
+                if prefix != "bt":
+                    raise NotImplementedError(channels)
+
+            # assuming all channels are brightness temperature we apply the
+            # same scaling for all channels, from 260K to 300K
+            scaling = (260, 300)
+
+            da_rgba = make_rgb(
+                da_scene,
+                invert_values=True,
+                alpha=1.0,
+                scaling=scaling,
+                channel=list(channels.keys()),
+            )
 
             # PIL assumes image shape (h, w, channels), indexes from
             # bottom-left of image, no need for alpha values and requires
