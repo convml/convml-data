89abd0da3fb263cdaa5b87c70d271e495c4f795b
leif <leif@denby.eu>
2023-01-19 10:45:01 +0000

    minor tweaks

--- convml_data/sources/ceres_syn1deg_modis/__init__.py
+++ convml_data/sources/ceres_syn1deg_modis/__init__.py
@@ -4,6 +4,5 @@ from
 https://asdc.larc.nasa.gov/project/CERES/CER_SYN1deg-1Hour_Terra-Aqua-MODIS_Edition4A
 """
 
-from .derived_variables import compute_derived_variable  # noqa
 from .extract import extract_variable  # noqa
 from .pipeline import QueryForData  # noqa
--- convml_data/sources/ceres_syn1deg_modis/extract.py
+++ convml_data/sources/ceres_syn1deg_modis/extract.py
@@ -16,7 +16,12 @@ def extract_variable(task_input, var_name):
 
     # make some more sensible names
     new_names = {
-        v: ds[v].long_name.lower().replace(" ", "_").replace("-", "_")
+        v: ds[v]
+        .long_name.lower()
+        .replace(" ", "_")
+        .replace("-", "_")
+        .replace("(", "")
+        .replace(")", "")
         for v in ds.data_vars
     }
 
@@ -38,6 +43,16 @@ def extract_variable(task_input, var_name):
 
     if var_name in ds.data_vars:
         da = ds[var_name]
+    elif var_name == "toa_sw_cre":
+        da = calc_toa_sw_cre(ds=ds)
+    elif var_name == "toa_lw_cre":
+        da = calc_toa_lw_cre(ds=ds)
+    elif var_name == "toa_net_cre":
+        da_toa_sw_cre = calc_toa_sw_cre(ds=ds)
+        da_toa_lw_cre = calc_toa_lw_cre(ds=ds)
+        da = da_toa_sw_cre + da_toa_lw_cre
+        da.attrs["units"] = da_toa_lw_cre.units
+        da.attrs["long_name"] = "TOA NET CRE"
     else:
         raise Exception(
             f"Variable `{var_name}` not found in MODIS SYN1Deg dataset. "
@@ -48,6 +63,27 @@ def extract_variable(task_input, var_name):
 
     extra_attrs = ["INPUTPOINTER"]
     for attr in extra_attrs:
-        del da.attrs[attr]
+        if attr in da.attrs:
+            del da.attrs[attr]
 
     return da
+
+
+def calc_toa_sw_cre(ds):
+    da_toa_sw_cre = (
+        ds.observed_clear_sky_toa_albedo - ds.observed_all_sky_toa_albedo
+    ) * ds.toa_sw_insolation
+    da_toa_sw_cre.attrs["units"] = ds.toa_sw_insolation.units
+    da_toa_sw_cre.attrs["long_name"] = "TOA SW CRE"
+    da_toa_sw_cre.name = "toa_sw_cre"
+    return da_toa_sw_cre
+
+
+def calc_toa_lw_cre(ds):
+    da_toa_lw_cre = -(
+        ds.observed_all_sky_toa_lw_flux - ds.observed_clear_sky_toa_lw_flux
+    )
+    da_toa_lw_cre.attrs["units"] = ds.observed_clear_sky_toa_lw_flux.units
+    da_toa_lw_cre.attrs["long_name"] = "TOA LW CRE"
+    da_toa_lw_cre.name = "toa_lw_cre"
+    return da_toa_lw_cre
