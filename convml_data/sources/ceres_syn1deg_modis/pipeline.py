3b1e4d054b3ab1878de2231d2d6fe686a1a5a452
Leif Denby <leif@denby.eu>
2023-02-02 13:37:41 +0000

    Add CERES-SYN1deg datasource (#34)

---
+++ convml_data/sources/ceres_syn1deg_modis/pipeline.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import luigi
+
+from ...utils.luigi import DBTarget, XArrayTarget
+from .download import download_file, make_url
+from .query import get_available_files, parse_filename
+
+
+class QueryForData(luigi.Task):
+    t_start = luigi.DateMinuteParameter()
+    t_end = luigi.DateMinuteParameter()
+    data_type = luigi.OptionalParameter(default=None)
+    data_path = luigi.Parameter()
+
+    DB_NAME_FORMAT = "keys_{t_start:%Y%m%d%H%M}_{t_end:%Y%m%d%H%M}"
+
+    def run(self):
+        filenames = list(get_available_files(t_start=self.t_start, t_end=self.t_end))
+
+        Path(self.output().fn).parent.mkdir(exist_ok=True, parents=True)
+        self.output().write(filenames)
+
+    def output(self):
+        db_name = self.DB_NAME_FORMAT.format(
+            t_start=self.t_start,
+            t_end=self.t_end,
+        )
+        return DBTarget(path=self.data_path, db_type="yaml", db_name=db_name)
+
+    @staticmethod
+    def get_time(filename):
+        return parse_filename(fn=filename)["time"]
+
+
+class FetchSyn1DegFile(luigi.Task):
+    data_path = luigi.Parameter()
+    filename = luigi.Parameter()
+
+    def run(self):
+        file_info = parse_filename(fn=self.filename)
+        url = make_url(**file_info)
+        Path(self.output().path).parent.mkdir(exist_ok=True, parents=True)
+        download_file(url, self.output().path)
+
+    def output(self):
+        return XArrayTarget(Path(self.data_path) / self.filename)
--- tests/test_datasource_pipeline.py
+++ tests/test_datasource_pipeline.py
@@ -3,14 +3,28 @@ import socket
 from pathlib import Path
 
 import luigi
+import pytest
 
 from convml_data import DataSource
 from convml_data.pipeline import GenerateRegriddedScenes, GenerateTiles
+from convml_data.sources.ceres_syn1deg_modis.earthaccess_auth import auth as ea_auth
 
 # from convml_data.pipeline.embeddings.sampling import DatasetScenesTileEmbeddings
 
 EXAMPLE_FILEPATH = str(Path(__file__).parent / "example")
 HAS_JASMIN_ACCESS = socket.getfqdn() in ["thixo"]
+
+ea = ea_auth.Auth().login(strategy="netrc")
+HAS_EARTHDATA_ACCESS = ea_auth.Auth().login(strategy="netrc").authenticated
+
+
+def _data_source_available(source_name):
+    if source_name == "era5" and not HAS_JASMIN_ACCESS:
+        return False
+    elif source_name == "ceres_syn1deg_modis" and not HAS_EARTHDATA_ACCESS:
+        return False
+
+    return True
 
 
 def test_make_triplets():
@@ -25,10 +39,8 @@ def test_make_triplets():
 
     aux_products = list(datasource.aux_products.keys())
     for aux_product_name in aux_products:
-        if (
-            datasource.aux_products[aux_product_name]["source"] == "era5"
-            and not HAS_JASMIN_ACCESS
-        ):
+        product_source = datasource.aux_products[aux_product_name]["source"]
+        if not _data_source_available(product_source):
             continue
 
         tasks.append(
@@ -43,28 +55,29 @@ def test_make_triplets():
     # assert luigi.build(task_embs, local_scheduler=True)
 
 
-def test_make_regridded_domain_data():
+def _parse_example_aux_products():
     datasource = DataSource.load(EXAMPLE_FILEPATH)
+    aux_product_names = []
 
-    tasks = [
-        GenerateRegriddedScenes(
-            data_path=EXAMPLE_FILEPATH,
-        )
-    ]
-
-    aux_products = list(datasource.aux_products.keys())
-    for aux_product_name in aux_products:
-        if (
-            datasource.aux_products[aux_product_name]["source"] == "era5"
-            and not HAS_JASMIN_ACCESS
-        ):
+    for aux_product_name, kwargs in datasource.aux_products.items():
+        product_source = datasource.aux_products[aux_product_name]["source"]
+        if not _data_source_available(product_source):
             continue
 
-        tasks.append(
-            GenerateRegriddedScenes(
-                data_path=EXAMPLE_FILEPATH,
-                aux_name=aux_product_name,
-            )
-        )
+        aux_product_names.append(aux_product_name)
+    return aux_product_names
 
-    assert luigi.build(tasks, local_scheduler=True)
+
+AUX_NAMES = [
+    None,
+] + _parse_example_aux_products()
+
+
+@pytest.mark.parametrize("aux_product_name", AUX_NAMES)
+def test_make_regridded_domain_data(aux_product_name):
+    task = GenerateRegriddedScenes(
+        data_path=EXAMPLE_FILEPATH,
+        aux_name=aux_product_name,
+    )
+
+    assert luigi.build([task], local_scheduler=True)
